53f2a3def816dc89ed72a53935eb9241a9298e30
WonderlandSwallowsAlice <mariageneralova73@gmail.com>
2026-09-12 15:46:15 -0700

    Test are fully working now and the output is correct. The output was not added to the generated out files seemingly to the string error that occured with passing in the seed. String issue was debugged by retyping 123 in the in files.

--- Cache.java
+++ Cache.java
@@ -7,11 +7,18 @@ public class Cache<K, V extends KeyInterface<K>> implements CacheInterface<K, V>
     private Integer references =0;
     private Integer hits=0; 
 
+    //Constructor for the cache class
     public Cache(int size) {
     this.cacheMaxSize = size; 
 	this.cache = new LinkedList<>();
     }
 
+    //Get function. Used for finding the object using the key.
+    //Loops through the cache and if finds it, moves it to the first position
+    //in the cache using addFirst(). While in the loop it also increments
+    //hits that later used to calculate the hit percentage. References are 
+    //incremented every time the get function is called. When the function 
+    //found the object it returns it, else it returns null.
     public @Override V get(K key)
     {
         references++; 
@@ -27,7 +34,11 @@ public class Cache<K, V extends KeyInterface<K>> implements CacheInterface<K, V>
         }
         return null; 
     }
-
+    
+    //Add function. Checks if the cache is full and if it is,
+    //removes LRU object using removeLast() and then adds the object
+    //to the first position using addFirst(). If the cache still has space,
+    //it adds the object to the first position and returns null. 
     public @Override V add(V value)
     {
         if (cache.size() >= cacheMaxSize)
@@ -44,6 +55,9 @@ public class Cache<K, V extends KeyInterface<K>> implements CacheInterface<K, V>
         }
     }
 
+    //Remove function. Goes throught the cache using the key to find
+    //the object associated with it. If it finds it, it removes it and
+    // returns it to the calling program. Else it returns null.
     public @Override V remove(K key)
     {
         for(int i = 0; i < cache.size(); i++)
@@ -58,11 +72,19 @@ public class Cache<K, V extends KeyInterface<K>> implements CacheInterface<K, V>
 
     }
 
+    //Clear function. Just clears all entries from the cache
+    // using clear(). 
     public @Override void clear()
     {
         cache.clear(); 
     }
  
+    //ToString Function. First calculates the hit percentage.
+    //Then changes it to 2 decimal places using String.format().
+    //Creates a string output that outputs the amount of entries in the cache,
+    //total references, total hits and the hit percentage.
+    //Returns that output to the calling program and displays
+    //in the terminal all the values and text. 
     public @Override String toString()
     {  
         double percentage = ((double)hits/references)*100.0; 
--- CacheExperiment.java
+++ CacheExperiment.java
@@ -1,85 +1,91 @@
-import java.util.HashMap;
-
 public class CacheExperiment
 {
     public static void main(String[] args) 
     {
+        //Variable to store the cache size, number of webpages, standard deviation and debug level arguments.
         int cacheSize = Integer.parseInt(args[0]);
         int numWebpages = Integer.parseInt(args[1]);
         double stanDev = Double.parseDouble(args[2]); 
         int debugLev = Integer.parseInt(args[3]);
 
+        //Stores the start time to calculate the elapsed time later. 
         long startTime = System.currentTimeMillis();
-            WebpageGenerator genPage;
-            if (args.length > 4)
-            {
-                long seed = Long.parseLong(args[4]);
-                genPage = new WebpageGenerator(numWebpages, stanDev, seed);
-            }
-            else
-            {
-                genPage = new WebpageGenerator(numWebpages, stanDev);
-            }
-        Cache<String, Webpage> cache = new Cache<>(cacheSize);
-        HashMap<String, Integer> dsnMap = new HashMap<>();
-       
-        for (int i = 0; i < numWebpages; i++)
-        { 
-            
-            String url = genPage.getURL();
-            Webpage found = cache.get(url);
-            if(dsnMap.containsKey(url))
-            {
-                dsnMap.put(url, dsnMap.get(url) + 1);
-            }
-            else
-            {
-                dsnMap.put(url, 1);
-            }
-            if(found == null)
-            {
-            Webpage newWeb = genPage.readPage(url); 
-            cache.add(newWeb);  
-            }
-        }    
 
+        WebpageGenerator genPage;
+
+        //Check for the amount of arguments passed in.
+        //If there is also a seed argument, it is added to the
+        //WebpageGenerator constructor. If there's no seed,
+        //it just uses numWebpages and stanDev for the constructor. 
+        if (args.length > 4)
+        {
+            long seed = Long.parseLong(args[4]);
+            genPage = new WebpageGenerator(numWebpages, stanDev, seed);
+        }
+        else
+        {
+            genPage = new WebpageGenerator(numWebpages, stanDev);
+        }
+
+        //Creates new cache object with the specified cache size.
+        Cache<String, Webpage> cache = new Cache<>(cacheSize);
+
+        for (int i = 0; i < numWebpages; i++)
+        {
+            String url = genPage.getURL();
+            Webpage page = cache.get(url);
+
+            if (page == null)
+            {
+                cache.add(genPage.readPage(url));
+            }
+        }
+
+        //Chooses one of 4 options based on the degug level argument passed in.
         switch (debugLev)
         {
+            //Case 0: prints the stats.
             case 0:
                 System.out.println(cache);
                 break;
+            //Case 1: prints the webpage distribution using
+            //getWebpageDatabasePings() provided by the WebpageGenerator class.
+            //It is followed by the stats.
             case 1:
                 System.out.println("-----------------------------------------\n");
                 System.out.println("Printing the Webpage Distribution:\n");
                 System.out.println("-----------------------------------------\n");
-                for (HashMap.Entry<String, Integer> entry : dsnMap.entrySet())
-                {
-                    System.out.println(entry.getKey() + " : " + entry.getValue());
-                }
+                genPage.getWebpageDatabasePings();
                 System.out.println(cache);
                 break;
+            //Case 2: print the url and the summarized content of each
+            //webpage using printWebpages() with the debug level 2 argument
+            //provided by the WebpageGenerator class. Followed by the stats. 
             case 2:
                System.out.println("=========================================\n");
                System.out.println("Generated and serialized Webpages\n");
                System.out.println("=========================================\n");
                genPage.printWebpages(2);
                 break;
-
+            //Case 3: similar to case 2, but prints the full content of the webpages
+            //using printWebpages() with the debug level 3 argument provided by the 
+            //WebpageGenerator class. 
             case 3:
                System.out.println("=========================================\n");
                System.out.println("URLs and Full Webpages\n");
                System.out.println("=========================================\n");
                genPage.printWebpages(3);
                 break;
+            //Default case: if user enters invalid debug level. 
             default:
                 System.out.println("Invalid debug level. Should be 0-3. Try again.");
                 break;
 
                 
         }
-
-
+        //Stores current time at the end of the operations being completed.
         long currentTime = System.currentTimeMillis();
+        //Calculates times elapsed and prints it to the terminal. 
         long timeElapsed = currentTime - startTime; 
         System.out.println("Time elapsed: " + timeElapsed + " milliseconds");
     }
